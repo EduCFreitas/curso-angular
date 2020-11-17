350e2c2e5680a9ef9458223b4136ab553b88c9d8
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-11-17 12:57:53 -0300

    Aula 7: Introdução aos serviços e injeção de dependências

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
@@ -9,10 +10,12 @@ export class CursosComponent implements OnInit {
 
   nomePortal: string;
 
-  cursos: string[] = ['Java', 'EXT Js', 'Angular'];
+  cursos: string[];
 
-  constructor() {
+  constructor(cursosService: CursosService) {
     this.nomePortal = 'http://loiane.training';
+
+    this.cursos = cursosService.getCursos();
    }
 
   ngOnInit(): void {
