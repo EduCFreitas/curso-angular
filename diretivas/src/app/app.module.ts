cabb53274ffe390570f7a4fe85d15b8ddcd4bf18
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-11-19 15:21:21 -0300

    Aula 29: Diretivas - ngClass

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
 import { FormsModule } from '@angular/forms';
 import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
+import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
     DiretivaNgswitchComponent,
-    DiretivaNgforComponent
+    DiretivaNgforComponent,
+    DiretivaNgclassComponent
   ],
   imports: [
     BrowserModule,
