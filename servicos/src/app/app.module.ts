dab3d84aa6cc762e3c48405afaec9d8567ccc829
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-11-20 12:08:22 -0300

    Aula 40: Escopo de instâncias de serviços e módulos (singleton e várias instâncias)

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -2,18 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CursosComponent } from './cursos/cursos.component';
 import { FormsModule } from '@angular/forms';
 import { CursosService } from './cursos/cursos.service';
+import { CriarCursoModule } from './criar-curso/criar-curso.module';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CursosComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    CriarCursoModule,
+    CursosModule
   ],
   providers: [CursosService],
   bootstrap: [AppComponent]
