c4a66362198b2960461f9608a6098c76540ef1f6
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-11-24 10:25:40 -0300

    Aula 58: Criando um módulo de funcionalidade

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -7,31 +7,33 @@ import { MaterializeModule } from 'angular2-materialize';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
+//import { CursosComponent } from './cursos/cursos.component';
 //import { routing } from './app.routing';
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
-import { CursosService } from './cursos/cursos.service';
-import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+//import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
+//import { CursosService } from './cursos/cursos.service';
+//import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 import { AppRoutingModule } from './app.routing.module';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent,
-    CursoDetalheComponent,
-    CursoNaoEncontradoComponent
+    //CursosComponent,
+    //CursoDetalheComponent,
+    //CursoNaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     MaterializeModule,
+    CursosModule,
     AppRoutingModule
     //routing
   ],
   providers: [
-    CursosService
+    //CursosService
   ],
   bootstrap: [AppComponent]
 })
--- rotas/src/app/app.routing.module.ts
+++ rotas/src/app/app.routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
-import { ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
 import { CursosComponent } from './cursos/cursos.component';
-import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 
 const appRoutes: Routes = [
     { path: 'cursos', component: CursosComponent },
