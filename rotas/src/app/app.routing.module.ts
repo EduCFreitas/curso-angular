e4e0c9bc4f82540521e8fc9d2a9a743c1c9295be
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-11-25 10:46:41 -0300

    Aula 64: Usando guarda de rotas - CanActivate

--- rotas/src/app/app.routing.module.ts
+++ rotas/src/app/app.routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from './guards/auth.guard';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -8,13 +9,22 @@ import { LoginComponent } from './login/login.component';
 //import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 
 const appRoutes: Routes = [
-    { path: 'cursos', loadChildren: () => import('./cursos/cursos.module').then(mod => mod.CursosModule) },
-    { path: 'alunos', loadChildren: () => import('./alunos/alunos.module').then(mod => mod.AlunosModule) },
+    { path: 'cursos',
+        loadChildren: () => import('./cursos/cursos.module').then(mod => mod.CursosModule),
+        canActivate: [AuthGuard] 
+    },
+    { path: 'alunos',
+        loadChildren: () => import('./alunos/alunos.module').then(mod => mod.AlunosModule),
+        canActivate: [AuthGuard]
+    },
     //{ path: 'cursos', component: CursosComponent },
     //{ path: 'curso/:id', component: CursoDetalheComponent },
     //{ path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
     { path: 'login', component: LoginComponent },
-    { path: '', component: HomeComponent }
+    { path: '',
+        component: HomeComponent,
+        canActivate: [AuthGuard]
+    }
 ];
 
 @NgModule({
