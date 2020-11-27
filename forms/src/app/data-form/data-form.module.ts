fcb665bcbfa38d5fad35f352507f25513f9a10c1
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-11-27 09:43:14 -0300

    Aula 92: Formulários reativos - Fazendo submit

--- forms/src/app/data-form/data-form.module.ts
+++ forms/src/app/data-form/data-form.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataFormComponent } from './data-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { SharedModule } from '../shared/shared.module';
-
-
 
 @NgModule({
   declarations: [
@@ -14,6 +13,7 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    HttpClientModule,
     SharedModule
   ]
 })
