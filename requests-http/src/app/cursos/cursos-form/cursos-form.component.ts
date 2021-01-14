536c1be86b0ef1322f23a287826e61a04c4e6021
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2021-01-14 17:06:34 -0300

    Aula 131: Http PUT - Atualizando cursos

--- requests-http/src/app/cursos/cursos-form/cursos-form.component.ts
+++ requests-http/src/app/cursos/cursos-form/cursos-form.component.ts
@@ -75,14 +75,41 @@ export class CursosFormComponent implements OnInit {
     console.log(this.form.value);
     if(this.form.valid){
       console.log('submit');
-      this.service.create(this.form.value).subscribe(
+
+      let msgSuccess = 'Curso criado com sucesso!';
+      let msgError = 'Erro ao criar curso. Tente novamente';
+      if(this.form.value.id){
+        msgSuccess = 'Curso atualizado com sucesso';
+        msgError = 'Erro ao atualizar curso. Tente novamente';
+      }
+
+      this.service.save(this.form.value).subscribe(
         success => {
-          this.modal.showAlertSuccess('Curso criado com sucesso');
+          this.modal.showAlertSuccess(msgSuccess);
           this.location.back();
         },
-        error => this.modal.showAlertDanger('Erro a criar curso. Tente novamente'),
-        () => console.log('request completo')
+        error => this.modal.showAlertDanger(msgError)
       );
+
+      // if(this.form.value.id){
+      //   this.service.update(this.form.value).subscribe(
+      //     success => {
+      //       this.modal.showAlertSuccess('Curso atualizado com sucesso');
+      //       this.location.back();
+      //     },
+      //     error => this.modal.showAlertDanger('Erro ao atualizar curso. Tente novamente'),
+      //     () => console.log('update completo')
+      //   );
+      // }else{
+      //   this.service.create(this.form.value).subscribe(
+      //     success => {
+      //       this.modal.showAlertSuccess('Curso criado com sucesso');
+      //       this.location.back();
+      //     },
+      //     error => this.modal.showAlertDanger('Erro ao criar curso. Tente novamente'),
+      //     () => console.log('request completo')
+      //   );
+      // }
     }
   }
 
